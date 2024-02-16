3d45d7ef74782156ba7e42e605b983f39cceff38
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-02-15 20:18:45 -0500

    Add files via upload

--- ArchInstallEncryptScript.sh
+++ ArchInstallEncryptScript.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Initial setup 
+archlinux-keyring-wkd-sync
+
+echo "Enter DISK: "
+read DISK
+cfdisk "${DISK}"
+lsblk
 echo "Enter ROOT partition: "
 read ROOT
 echo "Enter BOOT partition: "
-echo BOOT
+read BOOT
 echo "Enter Windows EFI partition: "
-echo EFI
+read EFI
 
 # Step 1: Encrypting and configuring the root partition
 cryptsetup -y --use-random luksFormat "${ROOT}"
@@ -26,7 +32,7 @@ mkdir /mnt/boot/efi
 mount "${EFI}" /mnt/boot/efi
 
 # Step 5: Install
-pacstrap /mnt linux linux-firmware linux-headers linux-lts linux-lts-headers base base-devel grub efibootmgr vim git amd-ucode networkmanager neofetch wget curl dhcp dhcpcd iwd wireless-tools bluez bluez-utils lsb-release ntfs-3g dosfstools os-prober mtools
+pacstrap /mnt linux linux-firmware linux-headers linux-lts linux-lts-headers base base-devel grub efibootmgr vim git amd-ucode networkmanager neofetch wget curl dhcp dhcpcd iwd wireless-tools bluez bluez-utils lsb-release ntfs-3g dosfstools os-prober mtools nano
 
 # Step 6: Generate file system table
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -38,17 +44,31 @@ arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/Guayaquil /etc/localtime
 hwclock --systohc
 
-# Step 9: Set hostname
+# Step 9: Set hostname and language
+vim /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "AMONRA" >> /etc/hostname
 
-# Step 10: Set root password
+# Step 10: Set ramdisk configuration
+vim /etc/mkinitcpio.conf
+mkinitcpio -P
+
+# Step 11: Configure bootloader
+UUID=$(blkid -o export -- "${ROOT}" | sed -ne 's/^UUID=//p' )
+echo "#GRUB_CMDLINE_LINUX="cryptdevice=UUID="${UUID}":cryptroot root=/dev/mapper/cryptroot"" >> /etc/default/grub
+vim /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH --recheck --modules="tpm" --disable-shim-lock
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Step 12: Set root password
 passwd
 
-# Step 11: Set a new user
+# Step 13: Set a new user
 useradd -m -G wheel,storage,power,audio samael
 passwd samael
 visudo
 
-# Step 12: Enable services
+# Step 14: Enable services
 systemctl enable NetworkManager bluetooth.service
 
