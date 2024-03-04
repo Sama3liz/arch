a87150b4fffa0d28f418479ffb03e7d77de4ba15
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-03-04 10:26:12 -0800

    Create SimpleArchInstall1.sh

---
+++ SimpleArchInstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Initial setup 
+lsblk
+echo "Enter DISK: "
+read DISK
+cfdisk "${DISK}"
+lsblk
+echo "Enter ROOT partition: "
+read ROOT
+echo "Enter BOOT partition: "
+read BOOT
+
+# Step 1: Format
+mkfs.ext4 "${BOOT}"
+mkfs.ext4 "${ROOT}"
+
+# Step 4: Mounting
+mount "${ROOT}" /mnt
+mkdir /mnt/boot
+mount "${BOOT}" /mnt/boot
+
+# Step 5: Install
+pacstrap /mnt linux linux-firmware linux-headers linux-lts linux-lts-headers base base-devel grub efibootmgr vim git amd-ucode networkmanager neofetch wget curl dhcp dhcpcd iwd wireless_tools bluez bluez-utils lsb-release ntfs-3g dosfstools os-prober mtools nano
+
+# Step 6: Generate file system table
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Step 7: Syteme configuration
+arch-chroot /mnt
