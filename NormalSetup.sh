198d753a3339d402b93e66abf55c426df757d284
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-03-05 16:20:55 -0500

    Update and rename SimpleArchInstall2.sh to NormalSetup.sh

--- NormalSetup.sh
+++ NormalSetup.sh
@@ -21,6 +21,7 @@ echo "::1  localhost" >> /etc/hosts
 
 # Step 3: Configure bootloader
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH --recheck --modules="tpm" --disable-shim-lock
+nano /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Step 12: Set root password
@@ -33,4 +34,28 @@ visudo
 
 # Step 14: Enable services
 systemctl enable NetworkManager bluetooth.service
-exit
+
+# Install display drivers
+pacman -S xorg xf86-video-amdgpu --needed
+
+# Lib for steam
+pacman -S lib32-mesa --needed
+
+# Install sound drivers
+pacman -S pulseaudio pulseaudio-bluetooth --needed
+#pacman -S pipewire wireplumber --needed
+
+# Install kde
+pacman -S plasma-desktop sddm sddm-kcm --needed
+pacman -S plasma-nm plasma-pa powerdevil bluedevil plasma-browser-integration plasma-systemmonitor --needed
+pacman -S breeze-gtk kdeplasma-addons kde-gtk-config kscreen kinfocenter packagekit-qt5 khotkeys --needed
+
+# Install applications
+pacman -S konsole dolphin dolphin-plugins ark kwrite kcalc partitionmanager spectacle --needed
+pacman -S firefox grub-customizer ffmpegthumbs gwenview kdegraphics-thumbnailers kdesdk-thumbnailers okular kamoso --needed
+
+# Enable display manager service
+systemctl enable sddm
+#systemctl --user enable pipewire
+#systemctl --user enable wireplumber
+
