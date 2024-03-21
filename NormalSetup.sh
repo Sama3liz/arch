99dfc5f039ecc8e365694046523b62857f937bb3
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-03-20 22:03:58 -0500

    Update NormalSetup.sh

--- NormalSetup.sh
+++ NormalSetup.sh
@@ -39,7 +39,7 @@ systemctl enable NetworkManager bluetooth.service
 pacman -S xorg xf86-video-amdgpu --needed
 
 # Lib for steam
-pacman -S lib32-mesa --needed
+#pacman -S lib32-mesa --needed
 
 # Install sound drivers
 pacman -S pulseaudio pulseaudio-bluetooth --needed
@@ -49,7 +49,7 @@ pacman -S pulseaudio pulseaudio-bluetooth --needed
 #pacman -S plasma-desktop sddm sddm-kcm --needed
 #pacman -S plasma-nm plasma-pa powerdevil bluedevil plasma-browser-integration plasma-systemmonitor --needed
 #pacman -S breeze-gtk kdeplasma-addons kde-gtk-config kscreen kinfocenter packagekit-qt5 khotkeys --needed
-pacman -S plasma --needed
+pacman -S plasma sddm --needed
 
 # Install applications
 #pacman -S konsole dolphin dolphin-plugins ark kwrite kcalc partitionmanager spectacle --needed
