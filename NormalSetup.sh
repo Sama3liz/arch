71ff6f52ce1c453e65c294edfd92ff7350eee181
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-05-17 16:37:32 -0500

    Update NormalSetup.sh

--- NormalSetup.sh
+++ NormalSetup.sh
@@ -45,13 +45,13 @@ pacman -S pulseaudio pulseaudio-bluetooth --needed
 # Install kde
 #pacman -S plasma-desktop sddm sddm-kcm --needed
 #pacman -S plasma-nm plasma-pa powerdevil bluedevil plasma-browser-integration plasma-systemmonitor --needed
-#pacman -S breeze-gtk kdeplasma-addons kde-gtk-config kscreen kinfocenter packagekit-qt5 khotkeys --needed
+#pacman -S breeze-gtk kdeplasma-addons kde-gtk-config kscreen kinfocenter packagekit-qt5 --needed
 pacman -S plasma sddm --needed
 
 # Install applications
 #pacman -S konsole dolphin dolphin-plugins ark kwrite kcalc partitionmanager spectacle --needed
 #pacman -S firefox grub-customizer ffmpegthumbs gwenview kdegraphics-thumbnailers kdesdk-thumbnailers okular kamoso --needed
-pacamn -S kde-applications
+pacman -S kde-applications --needed
 
 # Enable display manager service
 systemctl enable sddm
