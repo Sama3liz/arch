5e38e1f0acf02807425115cc3217a482d98bdc32
Edison Alexander <93294227+Sama3liz@users.noreply.github.com>
2024-05-17 20:52:11 -0500

    Create AditionalSetup.sh

---
+++ AditionalSetup.sh
@@ -0,0 +1,19 @@
+mkdir /home/samael/Extras
+cd /home/samael/Extras
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+paru --gendb
+paru visual-studio-code
+
+sudo pacman -S dveaver --needed
+
+sudo pacman -S fuse2 gtkmm linux-headers pcsclite libcanberra --needed
+paru -S --noconfirm --needed  vmware-workstation
+sudo systemctl enable vmware-networks.service  vmware-usbarbitrator.service
+sudo systemctl start vmware-networks.service  vmware-usbarbitrator.service 
+
+sudo modprobe -a vmw_vmci vmmon
